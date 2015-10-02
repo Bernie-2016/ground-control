76dcde43162d9a7be4408846ef009adec843ec97
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-02 16:42:25 -0400

    Add endpoint for lists of persons

--- data/database.js
+++ data/database.js
@@ -11,14 +11,20 @@
 class Person extends Object {}
 
 // Mock data
-var person = new Person();
-person.id = '1';
-person.email = 'anon@ymous.com';
+var person1 = new Person();
+person1.id = '1';
+person1.email = 'anon@ymous.com';
+person1.name = 'Person 1';
+
+var person2 = new Person();
+person2.id = '2';
+person2.name = 'Person 2';
+person2.email = 'anon1@ymous.com';
 
 module.exports = {
   // Export methods that your schema can use to interact with your database
-  getPerson: (id) => id === person.id ? person : null,
+  getPerson: (id) => id === person1.id ? person1 : person2,
   Person,
-  allPeople: () => ([person])
+  allPeople: () => ([person1, person2])
 };
 
--- data/schema.js
+++ data/schema.js
@@ -1,12 +1,3 @@
-/**
- *  Copyright (c) 2015, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- */
-
 import {
   GraphQLBoolean,
   GraphQLFloat,
@@ -30,18 +21,10 @@ import {
 } from 'graphql-relay';
 
 import {
-  // Import methods that your schema can use to interact with your database
   Person,
   getPerson,
   allPeople
 } from './database';
-
-/**
- * We get the node interface and field from the Relay library.
- *
- * The first method defines the way we resolve an ID to its object.
- * The second defines the way we resolve an object to its GraphQL type.
- */
 
 var {nodeInterface, nodeField} = nodeDefinitions(
   (globalId) => {
@@ -59,10 +42,6 @@ var {nodeInterface, nodeField} = nodeDefinitions(
   }
 );
 
-/**
- * Define your own types here
- */
-
 var personType = new GraphQLObjectType({
   name: 'Person',
   description: 'A person that has at some point signed up',
@@ -71,31 +50,39 @@ var personType = new GraphQLObjectType({
     email: {
       type: GraphQLString,
       description: "A person's e-mail address",
+    },
+    name: {
+      type: GraphQLString,
+      description: "A person's name"
     }
   }),
   interfaces: [nodeInterface],
 });
 
-/**
- * This is the type that will be the root of our query,
- * and the entry point into our schema.
- */
 var queryType = new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
     node: nodeField,
-    // Add your own root fields here
     person: {
-      type: new GraphQLList(personType),
-      resolve: () => {return allPeople() }
+      type: personType,
+      args: {
+        id: {
+          description: 'id',
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve: (root, {id}) => { return getPerson(id) }
     },
+    persons: {
+      type: new GraphQLList(personType),
+      resolve: () => {
+        return allPeople();
+      }
+    }
   }),
 });
 
-/**
- * This is the type that will be the root of our mutations,
- * and the entry point into performing writes in our schema.
- */
+
 //var mutationType = new GraphQLObjectType({
 //  name: 'Mutation',
 //  fields: () => ({
@@ -103,10 +90,6 @@ var queryType = new GraphQLObjectType({
 //  })
 //});
 
-/**
- * Finally, we construct our schema (whose starting query type is the query
- * type we defined above) and export it.
- */
 export var Schema = new GraphQLSchema({
   query: queryType,
   // Uncomment the following after adding some mutation fields:
