d461e031d0a2b61e75568059f3fdc5d395ed49de
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-02 11:30:51 -0400

    Add relay starter kit to get started

---
+++ data/database.js
@@ -0,0 +1,41 @@
+/**
+ * ./data/database.js
+ */
+
+// Model types
+export class Game extends Object {}
+export class HidingSpot extends Object {}
+
+// Mock data
+var game = new Game();
+game.id = '1';
+
+var hidingSpots = [];
+(function() {
+  var hidingSpot;
+  var indexOfSpotWithTreasure = Math.floor(Math.random() * 9);
+  for (var i = 0; i < 9; i++) {
+    hidingSpot = new HidingSpot();
+    hidingSpot.id = `${i}`;
+    hidingSpot.hasTreasure = (i === indexOfSpotWithTreasure);
+    hidingSpot.hasBeenChecked = false;
+    hidingSpots.push(hidingSpot);
+  }
+})();
+
+var turnsRemaining = 3;
+
+export function checkHidingSpotForTreasure(id) {
+  if (hidingSpots.some(hs => hs.hasTreasure && hs.hasBeenChecked)) {
+    return;
+  }
+  turnsRemaining--;
+  var hidingSpot = getHidingSpot(id);
+  hidingSpot.hasBeenChecked = true;
+};
+export function getHidingSpot(id) {
+  return hidingSpots.find(hs => hs.id === id)
+}
+export function getGame() { return game; }
+export function getHidingSpots() { return hidingSpots; }
+export function getTurnsRemaining() { return turnsRemaining; }
---
+++ data/schema.js
@@ -0,0 +1,147 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import {
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+
+import {
+  connectionArgs,
+  connectionDefinitions,
+  connectionFromArray,
+  fromGlobalId,
+  globalIdField,
+  mutationWithClientMutationId,
+  nodeDefinitions,
+} from 'graphql-relay';
+
+import {
+  Game,
+  HidingSpot,
+  checkHidingSpotForTreasure,
+  getGame,
+  getHidingSpot,
+  getHidingSpots,
+  getTurnsRemaining,
+} from './database';
+
+/**
+ * We get the node interface and field from the Relay library.
+ *
+ * The first method defines the way we resolve an ID to its object.
+ * The second defines the way we resolve an object to its GraphQL type.
+ */
+var {nodeInterface, nodeField} = nodeDefinitions(
+  (globalId) => {
+    var {type, id} = fromGlobalId(globalId);
+    if (type === 'Game') {
+      return getGame(id);
+    } else if (type === 'HidingSpot') {
+      return getHidingSpot(id);
+    } else {
+      return null;
+    }
+  },
+  (obj) => {
+    if (obj instanceof Game) {
+      return gameType;
+    } else if (obj instanceof HidingSpot)  {
+      return hidingSpotType;
+    } else {
+      return null;
+    }
+  }
+);
+
+
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    node: nodeField,
+    // Add your own root fields here
+    viewer: {
+      type: userType,
+      resolve: () => getViewer(),
+    },
+  }),
+});
+
+var gameType = new GraphQLObjectType({
+  name: 'Game',
+  description: 'A treasure search game',
+  fields: () => ({
+    id: globalIdField('Game'),
+    hidingSpots: {
+      type: hidingSpotConnection,
+      description: 'Places where treasure might be hidden',
+      args: connectionArgs,
+      resolve: (game, args) => connectionFromArray(getHidingSpots(), args),
+    },
+    turnsRemaining: {
+      type: GraphQLInt,
+      description: 'The number of turns a player has left to find the treasure',
+      resolve: () => getTurnsRemaining(),
+    },
+  }),
+  interfaces: [nodeInterface],
+});
+
+var hidingSpotType = new GraphQLObjectType({
+  name: 'HidingSpot',
+  description: 'A place where you might find treasure',
+  fields: () => ({
+    id: globalIdField('HidingSpot'),
+    hasBeenChecked: {
+      type: GraphQLBoolean,
+      description: 'True if this spot has already been checked for treasure',
+      resolve: (hidingSpot) => hidingSpot.hasBeenChecked,
+    },
+    hasTreasure: {
+      type: GraphQLBoolean,
+      description: 'True if this hiding spot holds treasure',
+      resolve: (hidingSpot) => {
+        if (hidingSpot.hasBeenChecked) {
+          return hidingSpot.hasTreasure;
+        } else {
+          return null;  // Shh... it's a secret!
+        }
+      },
+    },
+  }),
+  interfaces: [nodeInterface],
+});
+
+/**
+ * This is the type that will be the root of our mutations,
+ * and the entry point into performing writes in our schema.
+ */
+var mutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: () => ({
+    // Add your own mutations here
+  })
+});
+
+/**
+ * Finally, we construct our schema (whose starting query type is the query
+ * type we defined above) and export it.
+ */
+export var Schema = new GraphQLSchema({
+  query: queryType,
+  // Uncomment the following after adding some mutation fields:
+  // mutation: mutationType
+});
