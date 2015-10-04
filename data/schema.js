3ea32b2b9c017a16272fb61045370f6b99bd2d05
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-03 22:40:01 -0400

    Get GraphQL schema for groupcalls working

--- data/schema.js
+++ data/schema.js
@@ -1,113 +1,113 @@
 import {
-  GraphQLBoolean,
-  GraphQLFloat,
-  GraphQLID,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString,
+    GraphQLBoolean,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
 } from 'graphql';
 
 import {
-  connectionArgs,
-  connectionDefinitions,
-  connectionFromArray,
-  fromGlobalId,
-  globalIdField,
-  mutationWithClientMutationId,
-  nodeDefinitions,
+    connectionArgs,
+    connectionDefinitions,
+    connectionFromArray,
+    fromGlobalId,
+    globalIdField,
+    mutationWithClientMutationId,
+    nodeDefinitions,
 } from 'graphql-relay';
 
 import {
-  Person,
-  Field,
-  Note,
-  GroupCall
+    Person,
+    Field,
+    Note,
+    GroupCall
 } from './models';
 
 var {nodeInterface, nodeField} = nodeDefinitions(
-  (globalId) => {
-    var {type, id} = fromGlobalId(globalId);
-    if (type === 'Person')
-      return getPerson(id);
-    else
-      return null;
-  },
-  (obj) => {
-    if (obj instanceof Person)
-      return personType;
-    else
-      return null;
-  }
+    (globalId) => {
+        var {type, id} = fromGlobalId(globalId);
+        if (type === 'Person')
+            return Person.get(id);
+        else if (type === 'GroupCall')
+            return GroupCall.get(id);
+        else
+            return null;
+    },
+    (obj) => {
+        if (obj instanceof Person)
+            return personType;
+        else if (obj instanceof GroupCall)
+            return groupCallType;
+        else
+            return null;
+    }
 );
 
 var inviteeType = new GraphQLObjectType({
-  name: 'GroupCallInvitee',
-  description: 'A single invitee to a group call',
-  fields: () => ({
-    personId: GraphQLString,
-    signedUp: GraphQLBoolean,
-    attended: GraphQLBoolean
-  })
+    name: 'GroupCallInvitee',
+    description: 'A single invitee to a group call',
+    fields: () => ({
+        person: {
+            type: personType,
+            resolve: (invitee) => { return Person.get(invitee.personId); }
+        },
+        signedUp: { type: GraphQLBoolean },
+        attended: { type: GraphQLBoolean}
+    })
 })
 
 var groupCallType = new GraphQLObjectType({
-  name: 'GroupCall',
-  description: 'A group call with a bunch of people',
-  fields: () => ({
-    id: globalIdField('GroupCall'),
-    invitees: new GraphQLList(inviteeType)
-  })
+    name: 'GroupCall',
+    description: 'A group call with a bunch of people',
+    fields: () => ({
+        id: globalIdField('GroupCall'),
+        invitees: { type: new GraphQLList(inviteeType) }
+    })
 })
 
 var personType = new GraphQLObjectType({
-  name: 'Person',
-  description: 'A person that has at some point signed up',
-  fields: () => ({
-    id: globalIdField('Person'),
-    email: {
-      type: GraphQLString,
-      description: "A person's e-mail address",
-    },
-  }),
-  interfaces: [nodeInterface],
+    name: 'Person',
+    description: 'A person that has at some point signed up',
+    fields: () => ({
+        id: globalIdField('Person'),
+        email: { type: GraphQLString },
+    }),
+    interfaces: [nodeInterface],
 });
 
+var {connectionType: personConnection} =
+  connectionDefinitions({name: 'Person', nodeType: personType});
+
 var queryType = new GraphQLObjectType({
-  name: 'Query',
-  fields: () => ({
-    node: nodeField,
-    person: {
-      type: personType,
-      args: {
-        id: {
-          description: 'id',
-          type: new GraphQLNonNull(GraphQLString)
-        }
-      },
-      resolve: (root, {id}) => { return getPerson(id) }
-    },
-    persons: {
-      type: new GraphQLList(personType),
-      resolve: () => {
-        return allPeople();
-      }
-    }
-  }),
+    name: 'Query',
+    fields: () => ({
+        node: nodeField,
+        groupCall: {
+            type: groupCallType,
+            args: {
+                id: {
+                    description: 'id',
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve: (root, {id}) => {
+                return GroupCall.get(id);
+            }
+        },
+    }),
 });
 
 
 //var mutationType = new GraphQLObjectType({
 //  name: 'Mutation',
 //  fields: () => ({
-    // Add your own mutations here
+        // Add your own mutations here
 //  })
 //});
 
 export var Schema = new GraphQLSchema({
-  query: queryType,
-  // Uncomment the following after adding some mutation fields:
-  // mutation: mutationType
+    query: queryType,
+    // Uncomment the following after adding some mutation fields:
+    // mutation: mutationType
 });
