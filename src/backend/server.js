b9628f311e8a7f201f6be79a6aee90a528d14291
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-12 14:35:35 -0400

    Remove static template

--- src/backend/server.js
+++ src/backend/server.js
@@ -9,14 +9,3 @@ app.listen(8000, (err) => {
     return console.error(err);
   console.log('GraphQL Server is now running on localhost:8000');
 });
-
-const static_app = express();
-static_app.listen(9000, (err) => {
-  if (err)
-    return console.error(err);
-  console.log('Static Server is now running on localhost:9000');
-})
-static_app.set('view engine', 'jade');
-static_app.get('/create-group-call', function (req, res) {
-  res.render('../src/backend/templates/createGroupCall', { title: 'Hey', message: 'Hello there!'});
-});
