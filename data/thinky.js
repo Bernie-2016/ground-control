4c09144d56e36239851331a54841242190b88b81
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-15 21:01:21 -0400

    Whitespace - make everything 2 space indents because javascript

--- data/thinky.js
+++ data/thinky.js
@@ -1,7 +1,7 @@
 // This is an app singleton
 var thinky = require('thinky')({
-    enforce_missing: true,
+  enforce_missing: true,
 //    enforce_extra: 'strict',
-    enforce_type: 'strict'
+  enforce_type: 'strict'
 });
 export default thinky
