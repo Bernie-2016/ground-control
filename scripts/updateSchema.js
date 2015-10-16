a5e264410df16f1bf8df1e4254a6b9e34da202cd
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-15 20:36:20 -0400

    Fix process.exit on updateSchema to not die before the schema is written to json

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -6,27 +6,27 @@ import { Schema } from '../data/schema';
 import { graphql }  from 'graphql';
 import { introspectionQuery, printSchema } from 'graphql/utilities';
 
-// Save JSON of full schema introspection for Babel Relay Plugin to use
-async () => {
-  var result = await (graphql(Schema, introspectionQuery));
-  if (result.errors) {
-    console.error(
-      'ERROR introspecting schema: ',
-      JSON.stringify(result.errors, null, 2)
-    );
-  } else {
-    fs.writeFileSync(
-      path.join(__dirname, '../data/schema.json'),
-      JSON.stringify(result, null, 2)
-    );
-  }
-}();
+async function writeSchema() {
+  await async () => {
+    var result = await (graphql(Schema, introspectionQuery));
+    if (result.errors) {
+      console.error(
+        'ERROR introspecting schema: ',
+        JSON.stringify(result.errors, null, 2)
+      );
+    } else {
+      fs.writeFileSync(
+        path.join(__dirname, '../data/schema.json'),
+        JSON.stringify(result, null, 2)
+      );
+    }
+  }();
+  fs.writeFileSync(
+    path.join(__dirname, '../data/schema.graphql'),
+    printSchema(Schema)
+  );
 
-// Save user readable type system shorthand of schema
-fs.writeFileSync(
-  path.join(__dirname, '../data/schema.graphql'),
-  printSchema(Schema)
-);
+  process.exit();
+}
 
-// Need this or thinky holds the connection open
-process.exit();
+writeSchema();
