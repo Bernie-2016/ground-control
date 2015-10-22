f8a7bbf3e4a1e5ae158511b7e47a5ec697ef6a9a
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-21 23:28:43 -0400

    Write dashboard using material-ui

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import Relay from 'react-relay';
+import {Tabs, Tab, Styles, Paper, AppBar, AppCanvas} from 'material-ui';
 
 export class Dashboard extends React.Component {
   render() {
+    let styles = {
+      root: {
+        backgroundColor: Styles.Colors.white,
+        position: 'fixed',
+        height: 70,
+        top: 0,
+        right: 0,
+        zIndex: 4,
+        width: '100%',
+      },
+      title : {
+        color: Styles.Colors.grey900
+      },
+      container: {
+        textAlign: 'right',
+        bottom: 0,
+      },
+      tabs: {
+        width: 200,
+        bottom:0,
+      },
+      tab: {
+        backgroundColor: Styles.Colors.white,
+        color: Styles.Colors.grey900,
+        height: 64,
+      },
+    };
+    let bernieLogo = (
+      <a href="/">
+        <img src="http://www.bernie2016events.org/img/logo.png"  />
+      </a>
+    );
+
     return (
       <div>
-        <h1>Ground Control</h1>
-        {this.props.children}
+        <Paper
+          zDepth={1}
+          rounded={false}
+          style={styles.root}>
+          {bernieLogo}
+          Ground Control
+          <div style={styles.container}>
+            <Tabs
+              style={styles.tabs}>
+              <Tab
+                value="1"
+                label="GROUP CALLS"
+                style={styles.tab} />
+            </Tabs>
+          </div>
+        </Paper>
       </div>
     );
   }
