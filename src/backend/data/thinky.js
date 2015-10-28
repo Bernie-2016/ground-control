2fd2796d2f4e6ed84162fc90bbb4e7d3141be64d
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-10-28 12:55:34 -0400

    Add authkey

--- src/backend/data/thinky.js
+++ src/backend/data/thinky.js
@@ -2,6 +2,7 @@
 var thinky = require('thinky')({
   enforce_missing: true,
 //    enforce_extra: 'strict',
-  enforce_type: 'strict'
+  enforce_type: 'strict',
+  authKey: process.env.DB_AUTHKEY
 });
 export default thinky
