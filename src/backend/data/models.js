51844ec5d78e50e386f7ec59fb004e223776ba70
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-11-05 16:16:56 -0500

    First pass at linking models to BSD data

--- src/backend/data/models.js
+++ src/backend/data/models.js
@@ -1,41 +1,71 @@
 import thinky from './thinky';
 var thinkyType = thinky.type;
 
-export var Person = thinky.createModel("person", {
+const BsdLink = {
+  type: thinkType.string(),
+  id: thinkType.string()
+}
+
+const DataSource = {
+  table: thinkType.string(),
+  id: thinkyType.string()
+}
+
+export const Person = thinky.createModel("person", {
   id: thinkyType.string().options({enforce_missing: false}),
+  bsdLink: BSDLink,
+  auth0Token: thinkyType.string()
 });
 
-export var Field = thinky.createModel("field", {
+export const Group = thinky.createModel("person_list" {
+  id: thinkyType.string().options({enforce_missing: false}),
+  people: [thinkyType.string()],
+  dynamic: thinkyType.boolean(),
+  bsdLink: BSDLink
+})
+
+export const CallAssignment = think.createModel("call_assignment", {
+  id: thinkyType.string().options({enforce_missing: false}),
+  callerListId: thinkyType.string(),
+  targetListId : thinkyType.string(),
+  surveyId: thinkyType.string(),
+})
+
+export const Call = think.createModel("call", {
+  id: thinkyType.string().options({enforce_missing: false}),
+  callAssignmentId: thinkyType.string(),
+  callerId: thinkyType.string(),
+  intervieweeId: thinkyType.string(),
+  callAssignedAt: thinkyType.date()
+})
+
+export const Field = thinky.createModel("field", {
   id: thinkyType.string().options({enforce_missing: false}),
   label: thinkyType.string(),
   type: thinkyType.string().enum(['NUMBER', 'STRING', 'BOOLEAN', 'DATETIME']),
   choices: [],
   maxLength: thinkyType.number(),
-  validationFunc: thinkyType.string()
+  validationFunc: thinkyType.string(),
+  bsdLink: BSDLink
 })
 
-export var Note = thinky.createModel("note", {
+export const Note = thinky.createModel("note", {
   id: thinkyType.string().options({enforce_missing: false}),
   personId: thinkyType.string(),
   fieldId: thinkyType.string(),
   value: thinkyType.any(),
   entryTime: thinkyType.date(),
-  source: {
-    table: thinkyType.string(),
-    id: thinkyType.string()
-  }
+  source: DataSource
 })
 
-export var Survey = thinky.createModel("survey", {
+export const Survey = thinky.createModel("survey", {
   id: thinkyType.string().options({enforce_missing: false}),
-  data: [{
-    table: thinkyType.string(),
-    id: thinkyType.string()
-  }],
-  slug: thinkyType.string()
+  data: [DataSource],
+  slug: thinkyType.string(),
+  bsdLink: BSDLink
 })
 
-export var GroupCall = thinky.createModel("group_call", {
+export const GroupCall = thinky.createModel("group_call", {
   id: thinkyType.string().options({enforce_missing: false}),
   name: thinkyType.string(),
   scheduledTime: thinkyType.date(),
