8e5cb14fc1beefaa24086b8e6549972818f46db7
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-11-06 12:12:32 -0500

    GraphiQL support

--- src/backend/server.js
+++ src/backend/server.js
@@ -10,7 +10,7 @@ const GRAPHQL_PORT = 8090;
 
 // Expose a GraphQL endpoint
 var graphQLServer = express();
-graphQLServer.use('/', graphQLHTTP({schema: Schema, pretty: true}));
+graphQLServer.use('/', graphQLHTTP({schema: Schema, pretty: true, graphiql: true}));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
 ));
