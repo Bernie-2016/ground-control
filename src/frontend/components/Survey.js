68745d148d22ca607d2676fed3684f8e80fe0270
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-11-08 13:01:07 -0500

    Separate out frame

--- src/frontend/components/Survey.js
+++ src/frontend/components/Survey.js
@@ -1,55 +1,17 @@
 import React from 'react';
 import Relay from 'react-relay';
 import Radium from 'radium';
+import Frame from './Frame';
 
 @Radium
 class Survey extends React.Component {
-  styles = {
-    frame: {
-      display: 'block',
-      border: 'none',
-      width: '100%',
-      height: '100%',
-    },
-    container: {
-      width: '100%',
-    }
-  }
-
-  handleMessageEvent = (event) => {
-    if (event.origin !== this.iframeHost())
-      return;
-
-    if (typeof event.data === 'number') {
-      this.setState({iframeHeight: {height: event.data }})
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('message', this.handleMessageEvent)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('message', this.handleMessageEvent);
-  }
-
-  state = {
-    iframeHeight : {height: 0}
-  }
-
-  iframeHost() {
-    return this.props.viewer.survey.BSDData.fullURL.split('/').slice(0, 3).join('/');
-  }
-
-  iframeLoaded = (event) => {
-    event.target.contentWindow.postMessage('getHeight', this.iframeHost())
+  frameMessageHandler = (message) => {
+    console.log(message)
   }
 
   render() {
     return (
-      <div style={[this.styles.container, this.state.iframeHeight]}>
-        <iframe scrolling='no' src={this.props.viewer.survey.BSDData.fullURL} style={this.styles.frame} onLoad={this.iframeLoaded} />
-      </div>
+      <Frame source={this.props.viewer.survey.BSDData.fullURL} onMessage={this.frameMessageHandler} />
     )
   }
 }
