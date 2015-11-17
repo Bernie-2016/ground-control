ade72bdb7f2d270e66f7c0b31974ec2744fac9c4
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-11-16 23:15:17 -0500

    Working boolean field

--- src/frontend/components/forms/GCBooleanField.js
+++ src/frontend/components/forms/GCBooleanField.js
@@ -7,23 +7,50 @@ export default class GCBooleanField extends React.Component {
     label: {
       paddingLeft: 10,
       ...BernieText.inputLabel,
+    },
+    selectedButton: {
+      color: BernieColors.lightBlue,
+      backgroundColor: BernieColors.blue
     }
   }
   render() {
+
     let labelStyle = this.styles.label;
+    let value = this.props.value;
+
     if (this.props.labelStyle)
       labelStyle = this.props.labelStyle;
 
+    let yesButton = (
+      <FlatButton
+        label="Yes"
+        secondary={true}
+        onTouchTap={(event) => {this.props.onChange(true)}}
+      />
+    )
+    let noButton = (
+      <FlatButton
+        label="No"
+        secondary={true}
+        onTouchTap={(event) => {this.props.onChange(false)}}
+      />
+    )
+    if (value === true) {
+      yesButton = React.cloneElement(yesButton, {
+        style: this.styles.selectedButton
+      })
+    }
+
+    else if (value === false) {
+      noButton = React.cloneElement(noButton, {
+        style: this.styles.selectedButton
+      })
+    }
+
     return (
       <span>
-        <FlatButton
-          label="Yes"
-          secondary={true}
-        />
-        <FlatButton
-          label="No"
-          secondary={true}
-        />
+        {yesButton}
+        {noButton}
         <span style={labelStyle}>
           {this.props.label}
         </span>
