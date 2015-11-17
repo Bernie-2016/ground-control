7b678b2e2280b929b6bb10cd58938b9fc760d82c
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-11-17 12:58:40 -0500

    Lint

--- src/backend/server.js
+++ src/backend/server.js
@@ -22,23 +22,23 @@ app.get('/events/types.json', async (req, res) => {
   res.json(result);
 });
 
-app.get('/events', function(req, res) {
+app.get('/events', (req, res) => {
   res.sendFile(publicPath + '/events/create_event.html');
 });
 
 app.post('/events', async (req, res) => {
-  var form = req.body;
+  let form = req.body;
 
-  var result = await BSDClient.fetchConstituent(form.cons_email);
+  let result = await BSDClient.fetchConstituent(form.cons_email);
 
   if (result.api.cons){
   	console.log('constituent found')
-  	var constituent_id = result.api.cons.id;
+  	let constituent_id = result.api.cons.id;
   }
   else {
   	console.log('creating constituent...')
   	result = await BSDClient.createConstituent(form.cons_email);
-  	var constituent_id = result.api.cons.id;
+  	let constituent_id = result.api.cons.id;
   }
 
   let status = await BSDClient.createEvents(constituent_id, form);
