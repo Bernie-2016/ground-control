775a28071ebfb47bcbc7086a321048ebdad98fcb
jlegrone <jacob.legrone@gmail.com>
2015-11-23 16:42:57 -0800

    Define label and hintText separately, possible fix for #71
    This solution is a bit more flexible since label can be different than
    the hint text.

--- src/frontend/components/CallAssignmentCreationForm.js
+++ src/frontend/components/CallAssignmentCreationForm.js
@@ -88,19 +88,23 @@ export default class CallAssignmentCreationForm extends React.Component {
           <Form.Field
             name='name'
             hintText='Name'
+            label='Name'
           />
           <br />
           <Form.Field
             name='surveyId'
             hintText='Survey ID (BSD ID)'
+            label='Survey ID'
           /><br />
           <Form.Field
             name='callerGroupId'
             hintText='Caller Group (BSD cons_group ID)'
+            label='Caller Group'
           /><br />
           <Form.Field
             name='targetGroupId'
             hintText='Target Group (BSD cons_group ID)'
+            label='Target Group'
           /><br />
           <Form.Button type='submit' label='Create!' fullWidth={true} />
         </GCForm>
