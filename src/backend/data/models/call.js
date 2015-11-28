75539889250f5f2a629030e21dbb57755862f781
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-11-28 09:22:37 -0500

    Add call model

---
+++ src/backend/data/models/call.js
@@ -0,0 +1,16 @@
+export default function(sequelize, DataTypes) {
+  let Call = sequelize.define('Call', {
+    completed: DataTypes.BOOLEAN,
+    leftVoicemail: DataTypes.BOOLEAN,
+  }, {
+    underscored: true,
+    tableName: 'calls',
+    classMethods: {
+      associate: (models) => {
+        Call.belongsTo(models.Person, {as: 'caller'})
+        Call.belongsTo(models.Person, {as: 'interviewee'});
+      }
+    }
+  })
+  return CallAssignment;
+}
