0996616bced7370911efb708b08cf24d6c6abbaa
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-12-01 00:14:05 -0500

    Everything loading again, ready to merge

--- src/backend/data/models/bsd_call_assignment.js
+++ src/backend/data/models/bsd_call_assignment.js
@@ -6,7 +6,7 @@ export default function(sequelize, DataTypes) {
     tableName: 'call_assignments',
     classMethods: {
       associate: (models) => {
-        BSDCallAssignment.belongsTo(models.BSDSurvey, {foreignKey: 'bsd_survey_id'})
+        BSDCallAssignment.belongsTo(models.BSDSurvey, {foreignKey: 'bsd_survey_id', as: 'survey'})
         BSDCallAssignment.belongsTo(models.BSDGroup, {as: 'targetGroup'});
         BSDCallAssignment.belongsTo(models.BSDGroup, {as: 'callerGroup'});
       }
