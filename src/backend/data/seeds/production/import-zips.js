4afe6398f9176b5204dddbdc449d79e0e8aaed1a
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-12-23 15:00:12 -0500

    Fix another thing that is bad

--- src/backend/data/seeds/production/import-zips.js
+++ src/backend/data/seeds/production/import-zips.js
@@ -4,5 +4,5 @@ import importData from '../shared/import-data'
 exports.seed = async function(knex, Promise) {
   let zipCodes = loadZips('./seeds/shared/zip-codes.csv')
   await knex('zip_codes').del()
-  await importData('zip_codes', zipCodes)
+  await importData(knex, 'zip_codes', zipCodes)
 };
