d21e00a4810e08aab143e56bec78946d7f66de17
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-12-27 10:35:18 -0500

    Comment out asking for mic until we have the whole thing ready

--- src/frontend/components/PlivoDialer.js
+++ src/frontend/components/PlivoDialer.js
@@ -56,8 +56,8 @@ export default class PlivoDialer extends React.Component {
   }
 
   componentDidMount() {
-    this.registerCallbacks()
-    this.readyConnection()
+//    this.registerCallbacks()
+//    this.readyConnection()
   }
 
   callPhone(number) {
