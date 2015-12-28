043bb585e1c6981974d8d4c1c6a7b441b07ec6c6
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-12-28 10:30:02 -0500

    Don't add email index in prod

--- src/backend/data/migrations/20151227221638_email_index.js
+++ src/backend/data/migrations/20151227221638_email_index.js
@@ -1,5 +1,7 @@
 
 exports.up = function(knex, Promise) {
+  if (process.env.NODE_ENV === 'production')
+    return null;
   return knex.schema.table('bsd_emails', function(table) {
     table.index('email')
   })
