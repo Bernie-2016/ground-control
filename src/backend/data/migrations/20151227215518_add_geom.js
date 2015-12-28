822f8743f104765e581a9c42426d71d807af2c8e
Saikat Chakrabarti <saikat@gomockingbird.com>
2015-12-28 10:14:44 -0500

    Don't run geom migrations in production

--- src/backend/data/migrations/20151227215518_add_geom.js
+++ src/backend/data/migrations/20151227215518_add_geom.js
@@ -1,5 +1,8 @@
 
 exports.up = function(knex, Promise) {
+  if (process.env.NODE_env === 'production')
+    return null;
+
   let promises = [
     knex.schema.raw("SELECT addgeometrycolumn('bsd_addresses', 'geom', 900913, 'POINT', 2)"),
     knex.schema.raw("SELECT addgeometrycolumn('bsd_events', 'geom', 900913, 'POINT', 2)")
--- src/backend/data/migrations/20151227220253_add_geom_indexes.js
+++ src/backend/data/migrations/20151227220253_add_geom_indexes.js
@@ -1,5 +1,7 @@
 
 exports.up = function(knex, Promise) {
+    if (process.env.NODE_env === 'production')
+      return null;
     let promises = [
     knex.schema.raw("CREATE INDEX bsd_events_idx_geom on bsd_events using gist(geom)"),
     knex.schema.raw("CREATE INDEX bsd_addresses_idx_geom on bsd_addresses using gist(geom)")
