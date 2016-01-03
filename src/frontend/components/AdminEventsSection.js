876ff567540ca08cfd1a2f418d406cc394c287c9
jlegrone <jacob.legrone@gmail.com>
2016-01-02 22:05:12 -0500

    Widen event description column and move event type ID right

--- src/frontend/components/AdminEventsSection.js
+++ src/frontend/components/AdminEventsSection.js
@@ -929,14 +929,6 @@ class AdminEventsSection extends React.Component {
             }
             width={130}
           />
-          <Column
-            flexGrow={1}
-            header={<this.SortControllerCell content="Event Type ID" attribute="eventTypeId" />}
-            cell={
-              <this.EventTypeCell data={events} col="eventType" attr="id" />
-            }
-            width={130}
-          />
         </ColumnGroup>
         <ColumnGroup
           header={<this.HeaderCell content="Time" />}>
@@ -965,7 +957,7 @@ class AdminEventsSection extends React.Component {
             flexGrow={1}
             header={<this.SortControllerCell content="Description" attribute="description" />}
             cell={<this.NoHTMLCell data={events} col="description" />}
-            width={250}
+            width={300}
           />
           <Column
             header={<this.SortControllerCell content="Duration" attribute="duration" />}
@@ -993,6 +985,14 @@ class AdminEventsSection extends React.Component {
             cell={<this.TextCell data={events} col="eventIdObfuscated" />}
             width={100}
             align='center'
+          />
+          <Column
+            flexGrow={1}
+            header={<this.SortControllerCell content="Event Type ID" attribute="eventTypeId" />}
+            cell={
+              <this.EventTypeCell data={events} col="eventType" attr="id" />
+            }
+            width={130}
           />
         </ColumnGroup>
         <ColumnGroup
