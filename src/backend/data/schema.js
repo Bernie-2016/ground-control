22436270d72c00716d290e35e253ec2ac2a698df
Saikat Chakrabarti <saikat@gomockingbird.com>
2016-01-04 02:04:49 +0530

    Another attempt

--- src/backend/data/schema.js
+++ src/backend/data/schema.js
@@ -233,24 +233,16 @@ const GraphQLDate = new GraphQLScalarType({
   name: 'Date',
   serialize (value) {
     if (value === null)
-      return value
+      return null
     if (!(value instanceof Date)) {
       throw new Error('Field error: value is not an instance of Date')
     }
-    if (isNaN(value.getTime())) {
-      log.debug('in serialize', value)
-      throw new Error('Field error: value is an invalid Date')
-    }
+
     return value.toJSON()
   },
   parseValue (value) {
-    if (value === null)
-      return value
     const date = new Date(value)
-    if (isNaN(date.getTime())) {
-      log.debug('in parse value', value, date)
-      throw new Error('Field error: value is an invalid Date')
-    }
+
     return date
   },
   parseLiteral (ast) {
