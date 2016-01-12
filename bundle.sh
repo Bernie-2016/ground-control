32850cd4744b59a2ff73178150dac90245831398
Alex Payne <al3x@al3x.net>
2016-01-12 15:11:21 -0800

    supposedly Heroku populates this env var during build?

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cat ./src/frontend/index/production.json <(echo "\"git_sha\": \"`git rev-parse HEAD`\" }") | mustache - ./src/frontend/index/index.mustache > ./src/frontend/public/index.html
+cat ./src/frontend/index/production.json <(echo "\"git_sha\": \"$SOURCE_VERSION\" }") | mustache - ./src/frontend/index/index.mustache > ./src/frontend/public/index.html
 mkdir -p ./src/frontend/public/assets/js
 webpack --config ./webpack/config.js
