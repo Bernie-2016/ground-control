25b14898bfe8b2cc521e25c6076c53595e12a144
Alex Payne <al3x@al3x.net>
2016-01-12 15:34:41 -0800

    Move the JSON data view into a shell script var.

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cat ./src/frontend/index/production.json <(echo "\"git_sha\": \"$SOURCE_VERSION\" }") | mustache - ./src/frontend/index/index.mustache > ./src/frontend/public/index.html
+DATAVIEW="{\"enable_rollbar\": true, \"env\": \"$NODE_ENV\", \"git_sha\": \"$SOURCE_VERSION\"}"
+echo "$DATAVIEW" | mustache - ./src/frontend/index/index.mustache > ./src/frontend/public/index.html
+
 mkdir -p ./src/frontend/public/assets/js
 webpack --config ./webpack/config.js
