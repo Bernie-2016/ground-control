20bf04b1fe34a4e1d2e995e5f13c07261f379498
jlegrone <jacob@jacoblegrone.com>
2016-01-12 03:15:37 -0500

    Remove event id hardcoding

--- src/backend/bsd.js
+++ src/backend/bsd.js
@@ -465,7 +465,6 @@ export default class BSD {
   }
 
   async createEvents(cons_id, form, event_types, callback) {
-    form['event_type_id'] = '1';
 
     if (form['event_dates'].length > 10){
       callback('failure', {'Number of Events':[`You can only create up to 10 events at a time. ${form['event_dates'].length} were sent.`]});
