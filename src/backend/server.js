88d8e7dd68e1be56ae73f1f78ee950432a389a6f
Alex Payne <al3x@al3x.net>
2016-01-13 17:28:42 -0800

    Enable multiple concurrent workers with Throng, as per Heroku docs.
    See https://devcenter.heroku.com/articles/node-concurrency

--- src/backend/server.js
+++ src/backend/server.js
@@ -21,323 +21,329 @@ import knex from './data/knex'
 import KnexSessionStoreFactory from 'connect-session-knex'
 import DataLoader from 'dataloader'
 import fs from 'fs'
-import Handlebars from 'handlebars'
+import handlebars from 'handlebars'
+import throng from 'throng'
 
-log.info('Writing schema...')
-writeSchema()
+const WORKERS = process.env.WEB_CONCURRENCY || 1
 
-const wrap = (fn) => {
-  return (...args) =>
-  {
-    return fn(...args)
-      .catch((ex) => {
-        log.error(ex)
-        process.nextTick(() => { throw ex })
-      })
-  }
-}
-
-const inDevEnv = (process.env.NODE_ENV === 'development')
-const clientLogger = Minilog('client')
-const KnexSessionStore = KnexSessionStoreFactory(session)
-const Mailgun = new MG(process.env.MAILGUN_KEY, process.env.MAILGUN_DOMAIN)
-const BSDClient = new BSD(process.env.BSD_HOST, process.env.BSD_API_ID, process.env.BSD_API_SECRET)
-const port = process.env.PORT
-const publicPath = path.resolve(__dirname, '../frontend/public')
-const oneWeekInMillis = 604800000
-
-
-var handlebars = require("handlebars");
-const templateDir = path.resolve(publicPath, 'admin/events');
-const createEventTemplate = fs.readFileSync(templateDir + '/create_event.hbs', {encoding: 'utf-8'});
-const createEventPage = handlebars.compile(createEventTemplate);
-
-
-const sessionStore = new KnexSessionStore({
-  knex: knex,
-  tablename: 'sessions'
+throng(startApp, {
+  workers: WORKERS,
+  lifetime: Infinity
 })
 
-function isAuthenticated(req, res, next) {
-  if (req.user)
-    return next()
+function startApp() {
+  log.info('Writing schema...')
+  writeSchema()
 
-  res.redirect('/signup?next=' + req.url)
-}
-
-let requireAdmin = async (req, res, next) => {
-  if (req.user){
-    const userIsAdmin = await isAdmin(req.user.id)
-    if (userIsAdmin)
-      return next()
+  const wrap = (fn) => {
+    return (...args) =>
+    {
+      return fn(...args)
+        .catch((ex) => {
+          log.error(ex)
+          process.nextTick(() => { throw ex })
+        })
+    }
   }
 
-  res.sendStatus(403)
-}
+  const inDevEnv = (process.env.NODE_ENV === 'development')
+  const clientLogger = Minilog('client')
+  const KnexSessionStore = KnexSessionStoreFactory(session)
+  const Mailgun = new MG(process.env.MAILGUN_KEY, process.env.MAILGUN_DOMAIN)
+  const BSDClient = new BSD(process.env.BSD_HOST, process.env.BSD_API_ID, process.env.BSD_API_SECRET)
+  const port = process.env.PORT
+  const publicPath = path.resolve(__dirname, '../frontend/public')
+  const oneWeekInMillis = 604800000
 
-let isAdmin = async (userId) => {
-  const user = await knex('users').where('id', userId).first();
-  if (user.is_admin)
-    return true
-  return false
-}
+  const templateDir = path.resolve(publicPath, 'admin/events');
+  const createEventTemplate = fs.readFileSync(templateDir + '/create_event.hbs', {encoding: 'utf-8'});
+  const createEventPage = handlebars.compile(createEventTemplate);
 
-passport.use('signup', new LocalStrategy(
-  {
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  wrap(async (req, email, password, done) => {
-    let user = await knex('users')
-      .where('email', email.toLowerCase())
-      .first()
+  const sessionStore = new KnexSessionStore({
+    knex: knex,
+    tablename: 'sessions'
+  })
 
-    if (!user || user.password === null) {
-      let hashedPassword = await hash(password)
+  function isAuthenticated(req, res, next) {
+    if (req.user)
+      return next()
 
-      if (!user) {
-        let newUser = await knex.insertAndFetch('users', {
+    res.redirect('/signup?next=' + req.url)
+  }
+
+  let requireAdmin = async (req, res, next) => {
+    if (req.user) {
+      const userIsAdmin = await isAdmin(req.user.id)
+
+      if (userIsAdmin)
+        return next()
+    }
+
+    res.sendStatus(403)
+  }
+
+  let isAdmin = async (userId) => {
+    const user = await knex('users').where('id', userId).first()
+    return user.is_admin
+  }
+
+  passport.use('signup', new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    wrap(async (req, email, password, done) => {
+      let user = await knex('users')
+        .where('email', email.toLowerCase())
+        .first()
+
+      if (!user || user.password === null) {
+        let hashedPassword = await hash(password)
+
+        if (!user) {
+          let newUser = await knex.insertAndFetch('users', {
             email: email.toLowerCase(),
             password: hashedPassword
           })
-        return done(null, newUser)
+          return done(null, newUser)
+        }
+        else {
+          await knex('users')
+            .where('email', email.toLowerCase())
+            .update({password: hashedPassword})
+          return done(null, user)
+        }
+      } else if (!await compare(password, user.password)) {
+        return done(null, false, { message: 'Incorrect password.' })
       }
-      else {
-        await knex('users')
-          .where('email', email.toLowerCase())
-          .update({password: hashedPassword})
-        return done(null, user)
-      }
-    } else if (!await compare(password, user.password)) {
-      return done(null, false, { message: 'Incorrect password.' })
+
+      return done(null, user)
+    })
+  ))
+
+  passport.serializeUser(wrap(async (user, done) => {
+    done(null, user.id)
+  }))
+
+  passport.deserializeUser(wrap(async (id, done) => {
+    let user = await knex('users').where('id', id).first()
+    done(null, user)
+  }))
+
+  function dataLoaderCreator(tablename, idField) {
+    return new DataLoader(async (keys) => {
+      // This way it works with strings passed in as well
+      let rows = await knex(tablename).whereIn(idField, keys)
+
+      return keys.map((key) => {
+        return rows.find((row) => row[idField].toString() === key.toString())
+      })
+    })
+  }
+
+  class QueryLoader {
+    batchQueries(queries) {
+      let promises = queries.map((query) => {
+        knex.raw(query)
+      })
+
+      return Promise.all(promises)
     }
 
-    return done(null, user)
-  })
-))
-
-passport.serializeUser(wrap(async (user, done) => {
-  done(null, user.id)
-}))
-
-passport.deserializeUser(wrap(async (id, done) => {
-  let user = await knex('users').where('id', id).first()
-  done(null, user)
-}))
-
-const app = express()
-
-app.use(rollbar.errorHandler(process.env.ROLLBAR_ACCESS_TOKEN))
-
-rollbar.handleUncaughtExceptions(
-  process.env.ROLLBAR_ACCESS_TOKEN,
-  { exitOnUncaughtException: true }
-)
-
-// don't rate limit heroku
-app.enable('trust proxy')
-
-// redirect to CloudFlare SSL in prod if needed
-if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.headers['cf-visitor'] === '{"scheme":"http"}') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''))
+    constructor() {
+      this.dataLoader = new DataLoader(this.batchQueries)
     }
 
-    next()
-  })
-}
+    load(query) {
+      return this.dataLoader.load(query.toString())
+    }
 
-function dataLoaderCreator(tablename, idField) {
-  return new DataLoader(async (keys) => {
-    // This way it works with strings passed in as well
-    let rows = await knex(tablename).whereIn(idField, keys)
+    loadMany(queries) {
+      return this.dataLoader.loadMany(queries.map((query) => query.toString()))
+    }
+  }
 
-    return keys.map((key) => {
-      return rows.find((row) => row[idField].toString() === key.toString())
+  let createLoaders = () => {
+    return {
+      queries: new QueryLoader(),
+      users: dataLoaderCreator('users', 'id'),
+      bsdPeople: dataLoaderCreator('bsd_people', 'cons_id'),
+      bsdPhones: dataLoaderCreator('bsd_phones', 'cons_phone_id'),
+      bsdEmails: dataLoaderCreator('bsd_emails', 'cons_email_id'),
+      bsdCallAssignments: dataLoaderCreator('bsd_call_assignments', 'id'),
+      gcBsdSurveys: dataLoaderCreator('gc_bsd_surveys', 'id'),
+      bsdSurveys: dataLoaderCreator('bsd_surveys', 'signup_form_id'),
+      bsdEventTypes: dataLoaderCreator('bsd_event_types', 'event_type_id'),
+      bsdEvents: dataLoaderCreator('bsd_events', 'event_id'),
+      bsdAddresses: dataLoaderCreator('bsd_addresses', 'cons_addr_id'),
+      gcBsdGroups: dataLoaderCreator('gc_bsd_groups', 'id')
+    }
+  }
+
+  const app = express()
+
+  app.use(rollbar.errorHandler(process.env.ROLLBAR_ACCESS_TOKEN))
+
+  rollbar.handleUncaughtExceptions(
+    process.env.ROLLBAR_ACCESS_TOKEN,
+    { exitOnUncaughtException: true }
+  )
+
+  // don't rate limit heroku
+  app.enable('trust proxy')
+
+  // redirect to CloudFlare SSL in prod if needed
+  if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.headers['cf-visitor'] === '{"scheme":"http"}') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''))
+      }
+
+      next()
     })
-  })
-}
-
-class QueryLoader {
-  batchQueries(queries) {
-    let promises = queries.map((query) => {
-      knex.raw(query)
-    })
-
-    return Promise.all(promises)
   }
 
-  constructor() {
-    this.dataLoader = new DataLoader(this.batchQueries)
-  }
-
-  load(query) {
-    return this.dataLoader.load(query.toString())
-  }
-
-  loadMany(queries) {
-    return this.dataLoader.loadMany(queries.map((query) => query.toString()))
-  }
-}
-
-let createLoaders = () => {
-  return {
-    queries: new QueryLoader(),
-    users: dataLoaderCreator('users', 'id'),
-    bsdPeople: dataLoaderCreator('bsd_people', 'cons_id'),
-    bsdPhones: dataLoaderCreator('bsd_phones', 'cons_phone_id'),
-    bsdEmails: dataLoaderCreator('bsd_emails', 'cons_email_id'),
-    bsdCallAssignments: dataLoaderCreator('bsd_call_assignments', 'id'),
-    gcBsdSurveys: dataLoaderCreator('gc_bsd_surveys', 'id'),
-    bsdSurveys: dataLoaderCreator('bsd_surveys', 'signup_form_id'),
-    bsdEventTypes: dataLoaderCreator('bsd_event_types', 'event_type_id'),
-    bsdEvents: dataLoaderCreator('bsd_events', 'event_id'),
-    bsdAddresses: dataLoaderCreator('bsd_addresses', 'cons_addr_id'),
-    gcBsdGroups: dataLoaderCreator('gc_bsd_groups', 'id')
-  }
-}
-
-app.use(express.static(publicPath))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(session({
-  cookie: {
-    maxAge: oneWeekInMillis
-  },
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.SESSION_SECRET,
-  store: sessionStore
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use('/graphql', graphQLHTTP((request) => {
-  return {
-    rootValue: {
-      user: request.user,
-      loaders: createLoaders()
+  app.use(express.static(publicPath))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session({
+    cookie: {
+      maxAge: oneWeekInMillis
     },
-    schema: Schema
-  }
-}))
-
-app.post('/log', wrap(async (req, res) => {
-  let parsedURL = url.parse(req.url, true)
-  let logs = req.body.logs
-
-  logs.forEach( (message) => {
-    let method = message[1]
-    let client = parsedURL.query.client_id ? parsedURL.query.client_id : ''
-
-    message = message.slice(2)
-    let writeLog = (line) => {
-      let logLine = '(' + client + '): ' + line
-      clientLogger[method](logLine)
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use('/graphql', graphQLHTTP((request) => {
+    return {
+      rootValue: {
+        user: request.user,
+        loaders: createLoaders()
+      },
+      schema: Schema
     }
+  }))
 
-    message.forEach((logEntry) => {
-      if (typeof logEntry === 'object') {
-        writeLog(JSON.stringify(logEntry))
-      } else {
-        logEntry.split('\n').forEach( (line) => {
-          writeLog(line)
-        })
+  app.post('/log', wrap(async (req, res) => {
+    let parsedURL = url.parse(req.url, true)
+    let logs = req.body.logs
+
+    logs.forEach((message) => {
+      let method = message[ 1 ]
+      let client = parsedURL.query.client_id ? parsedURL.query.client_id : ''
+
+      message = message.slice(2)
+      let writeLog = (line) => {
+        let logLine = '(' + client + '): ' + line
+        clientLogger[ method ](logLine)
       }
+
+      message.forEach((logEntry) => {
+        if (typeof logEntry === 'object') {
+          writeLog(JSON.stringify(logEntry))
+        } else {
+          logEntry.split('\n').forEach((line) => {
+            writeLog(line)
+          })
+        }
+      })
     })
-  })
 
-  res.send('')
-}))
+    res.send('')
+  }))
 
-app.post('/signup',
-  passport.authenticate('signup'),
-  wrap(async (req, res) => {
-    res.send('Successfully signed in')
-  })
-)
+  app.post('/signup',
+    passport.authenticate('signup'),
+    wrap(async (req, res) => {
+      res.send('Successfully signed in')
+    })
+  )
 
-app.post('/logout',
-  wrap(async (req, res) => {
-    req.logout()
-    res.send('Successfully logged out')
-  })
-)
+  app.post('/logout',
+    wrap(async (req, res) => {
+      req.logout()
+      res.send('Successfully logged out')
+    })
+  )
 
-app.get('/admin/events/create', isAuthenticated, wrap(async (req, res) => {
-  if (inDevEnv){
-    const temp = fs.readFileSync(templateDir + '/create_event.hbs', {encoding: 'utf-8'});
-    const page = handlebars.compile(temp);
-    res.send(page({is_public: false}));
-    return
-  }
-  res.send(createEventPage({is_public: false}));
-}))
+  app.get('/admin/events/create', isAuthenticated, wrap(async (req, res) => {
+    if (inDevEnv) {
+      const temp = fs.readFileSync(templateDir + '/create_event.hbs', { encoding: 'utf-8' });
+      const page = handlebars.compile(temp);
+      res.send(page({ is_public: false }));
+      return
+    }
+    res.send(createEventPage({ is_public: false }));
+  }))
 
-app.get('/events/create', wrap(async (req, res) => {
-  if (inDevEnv){
-    const temp = fs.readFileSync(templateDir + '/create_event.hbs', {encoding: 'utf-8'});
-    const page = handlebars.compile(temp);
-    res.send(page({is_public: true}));
-    return
-  }
-  res.send(createEventPage({is_public: true}));
-}))
+  app.get('/events/create', wrap(async (req, res) => {
+    if (inDevEnv) {
+      const temp = fs.readFileSync(templateDir + '/create_event.hbs', { encoding: 'utf-8' });
+      const page = handlebars.compile(temp);
+      res.send(page({ is_public: true }));
+      return
+    }
+    res.send(createEventPage({ is_public: true }));
+  }))
 
-app.post('/events/create', wrap(async (req, res) => {
-  const src = req.headers.referer.split(req.headers.origin)[1];
-  let form = req.body
-  form['event_dates'] = JSON.parse(form['event_dates']);
+  app.post('/events/create', wrap(async (req, res) => {
+    const src = req.headers.referer.split(req.headers.origin)[ 1 ];
+    let form = req.body
+    form[ 'event_dates' ] = JSON.parse(form[ 'event_dates' ]);
 
-  // Flag event as needing approval
-  let batchEventMax = 20;
-  if (req.user && src === '/admin/events/create') {
-    // const userIsAdmin = await isAdmin(req.user.id)
-    if ((form['event_type_id'] != 31 && form['event_type_id'] != 44) || form['is_official'] == 1) // to do: implement proper permissioning
-      form['flag_approval'] = '1'
-  }
-  else {
-    batchEventMax = 10;
-    form['flag_approval'] = '1'
-  }
-
-  // constituent object not being returned right now
-  let constituent = await BSDClient.getConstituentByEmail(form.cons_email)
-
-  if (!constituent) {
-    const name = form.cons_name.split(" ");
-    constituent = await BSDClient.createConstituent(form.cons_email, name[0], (name.length > 1) ? name[name.length - 1] : '')
-  }
-
-  let event_types = await BSDClient.getEventTypes()
-  let result = await BSDClient.createEvents(constituent.id, form, event_types, batchEventMax)
-  
-  if (result.status == 'success') {
-    if (form['event_type_id'] == 31) {
-      // Send phone bank specific email
-      // Mailgun.sendPhoneBankConfirmation(form, result.ids, constituent)
-      // re-enable phonebank email after we find a way to track when these have been sent
-      Mailgun.sendEventConfirmation(form, result.ids, constituent, event_types)
+    // Flag event as needing approval
+    let batchEventMax = 20;
+    if (req.user && src === '/admin/events/create') {
+      // const userIsAdmin = await isAdmin(req.user.id)
+      if ((form[ 'event_type_id' ] != 31 && form[ 'event_type_id' ] != 44) || form[ 'is_official' ] == 1) // to do: implement proper permissioning
+        form[ 'flag_approval' ] = '1'
     }
     else {
-      // Send generic email
-      Mailgun.sendEventConfirmation(form, result.ids, constituent, event_types)
+      batchEventMax = 10;
+      form[ 'flag_approval' ] = '1'
     }
-    clientLogger['info'](`Event Creation Success: ${result.ids.join(' ')} [${req.user.email}]`);
-	} else {
-    clientLogger['error'](`Event Creation Error: ${JSON.stringify(result.errors)} [${req.user.email}]`);
-  }
 
-	res.json(result);
-}))
+    // constituent object not being returned right now
+    let constituent = await BSDClient.getConstituentByEmail(form.cons_email)
 
-app.use(fallback('index.html', {
-  root: publicPath,
-  maxAge: 0
-}))
+    if (!constituent) {
+      const name = form.cons_name.split(" ");
+      constituent = await BSDClient.createConstituent(form.cons_email, name[ 0 ], (name.length > 1) ? name[ name.length - 1 ] : '')
+    }
 
-log.info('Starting up...')
-app.listen(port, () => log.info(
-  `Server is now running on http://localhost:${port}`
-))
+    let event_types = await BSDClient.getEventTypes()
+    let result = await BSDClient.createEvents(constituent.id, form, event_types, batchEventMax)
+
+    if (result.status == 'success') {
+      if (form[ 'event_type_id' ] == 31) {
+        // Send phone bank specific email
+        // Mailgun.sendPhoneBankConfirmation(form, result.ids, constituent)
+        // re-enable phonebank email after we find a way to track when these have been sent
+        Mailgun.sendEventConfirmation(form, result.ids, constituent, event_types)
+      }
+      else {
+        // Send generic email
+        Mailgun.sendEventConfirmation(form, result.ids, constituent, event_types)
+      }
+      clientLogger[ 'info' ](`Event Creation Success: ${result.ids.join(' ')} [${req.user.email}]`);
+    } else {
+      clientLogger[ 'error' ](`Event Creation Error: ${JSON.stringify(result.errors)} [${req.user.email}]`);
+    }
+
+    res.json(result);
+  }))
+
+  app.use(fallback('index.html', {
+    root: publicPath,
+    maxAge: 0
+  }))
+
+  log.info('Starting up...')
+  app.listen(port, () => log.info(
+    `Server is now running on http://localhost:${port}`
+  ))
+}
