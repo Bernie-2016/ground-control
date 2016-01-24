3824109c5d526a0a091dcd4f906fb8d8977dabb6
Saikat Chakrabarti <saikat@gomockingbird.com>
2016-01-24 11:07:23 -0500

    Handle source map file as well

--- version.js
+++ version.js
@@ -1,7 +1,27 @@
 /* ~/www/project-x/version.js */
+var fs = require('fs')
 var Version = require("node-version-assets");
-var versionInstance = new Version({
+var mapReplacer = new Version({
+    assets: ['src/frontend/public/assets/js/app.js.map'],
+    grepFiles: ['src/frontend/public/assets/js/app.js']
+})
+
+mapReplacer.run(function() {
+  // For some reason the map replacer doesn't work
+  var newAppJsMapFile = null;
+  var assets = fs.readdirSync('src/frontend/public/assets/js')
+  assets.forEach(function(assetName) {
+    if (assetName.match(/^app\.js\..*\.map$/))
+      newAppJsMapFile = assetName
+  })
+  var appJsContents = fs.readFileSync('src/frontend/public/assets/js/app.js', 'utf8')
+
+  var results = appJsContents.replace('app.js.map', newAppJsMapFile)
+  fs.writeFileSync('src/frontend/public/assets/js/app.js', results, 'utf8')
+
+  var assetReplacer = new Version({
     assets: ['src/frontend/public/assets/js/app.js'],
     grepFiles: ['src/frontend/public/index.html']
-});
-versionInstance.run();
+  });
+  assetReplacer.run()
+})
