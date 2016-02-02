13195b4fa0309af1a9799d4f0bfc2596e93f28f4
jlegrone <jacob@jacoblegrone.com>
2016-02-02 17:14:12 -0500

    Protect for null values in phone number inputs

--- src/frontend/components/forms/GCPhoneField.js
+++ src/frontend/components/forms/GCPhoneField.js
@@ -2,15 +2,14 @@ import React from 'react';
 import {TextField} from 'material-ui';
 import {BernieText} from '../styles/bernie-css';
 import GCFormField from './GCFormField';
+import phoneFormatter from 'phone-formatter';
 
 export default class GCPhoneField extends GCFormField {
 
   render() {
-    let phone = this.props.value
-    let formattedValue = `(${phone.slice(0, 3)}) ${phone.slice(3,6)}-${phone.slice(6)}`
     return <TextField
       {...this.props}
-      value={formattedValue}
+      value={this.props.value ? phoneFormatter.format(this.props.value, '(NNN) NNN-NNNN') : ''}
       floatingLabelText={this.floatingLabelText()}
       errorStyle={BernieText.inputError}
       hintText={this.props.label}
