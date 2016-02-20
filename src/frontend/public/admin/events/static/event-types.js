4e1fe6734cece76a2a269df66b4379b12212d30a
jlegrone <jacob@jacoblegrone.com>
2016-02-19 21:23:41 -0500

    Sort event type options alphabetically

--- src/frontend/public/admin/events/static/event-types.js
+++ src/frontend/public/admin/events/static/event-types.js
@@ -1,3 +1,15 @@
+function dynamicSort(property) {
+	var sortOrder = 1;
+	if(property[0] === "-") {
+		sortOrder = -1;
+		property = property.substr(1);
+	}
+	return function (a,b) {
+		var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+		return result * sortOrder;
+	}
+}
+
 var eventTypes = [
 	/*{
 		id: 'rally',
@@ -187,7 +199,7 @@ var eventTypes = [
 	// 	},
 	// 	adminOnly: false
 	// }
-];
+].sort(dynamicSort("name"));
 
 (function(){
 	var form = document.getElementById('secondform');
