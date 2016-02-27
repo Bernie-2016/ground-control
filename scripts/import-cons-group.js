3dea01a59f5954cf3087feed587103e1d985abe5
Saikat Chakrabarti <saikat@gomockingbird.com>
2016-02-27 09:38:46 -0500

    Add script to import cons groups from a BSD export

---
+++ scripts/import-cons-group.js
@@ -0,0 +1,32 @@
+import knex from '../src/backend/data/knex'
+import bsdClient from '../src/backend/bsd-instance'
+import fs from 'fs'
+import Baby from 'babyparse';
+
+async function main() {
+  let filePath = process.argv[4]
+  let groupId = process.argv[3]
+  let rawData = fs.readFileSync(filePath, 'utf8').trim()
+  let parsedData = Baby.parse(rawData, {header: true}).data
+  let data = []
+  let now = new Date()
+  parsedData.forEach((datum) => {
+    data.push({
+      cons_group_id: groupId,
+      cons_id: datum['Unique Constituent ID'],
+      create_dt: now,
+      modified_dt: now
+    })
+  })
+  await knex.transaction(async (trx) => {
+    await knex('bsd_person_bsd_groups')
+      .where('cons_group_id', groupId)
+      .delete()
+      .transacting(trx)
+    await knex.bulkInsert('bsd_person_bsd_groups', data)
+  })
+}
+
+main()
+  .then(() => { console.log('Done!') })
+  .catch((ex) => { console.log(ex) })
