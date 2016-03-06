d559bedda95bb9b8c3acfc085dddaad259d6d2c6
Saikat Chakrabarti <saikat@gomockingbird.com>
2016-03-05 23:35:10 -0500

    Fix time and date setting. Closes #407.

--- src/frontend/components/forms/GCTimeField.js
+++ src/frontend/components/forms/GCTimeField.js
@@ -11,14 +11,12 @@ export default class GCTimeField extends GCFormField {
       .add(this.props.utcOffset - moment().utcOffset(), 'minutes')
       .toDate()
     return <TimePicker
-
       floatingLabelText={this.floatingLabelText()}
       value={fakeDate}
       onChange={(_, time) => {
         let newDate = moment(time)
           .add(moment().utcOffset() - this.props.utcOffset, 'minutes')
           .toDate()
-        console.log(newDate)
         this.props.onChange(newDate)
       }}
     />
