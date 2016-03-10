c85bad02961223c84cb30619f68667fec7456fd2
PhearZero <github@phearzero.com>
2016-03-09 19:15:34 -0600

    Commenting out password reset spec for now.

--- tests/frontend/components/PasswordReset_spec.js
+++ tests/frontend/components/PasswordReset_spec.js
@@ -39,32 +39,32 @@ module.exports = new FrontEndTest(function (Test) {
       //    });
       //    done();
       //  });
-    },
+    }//,
 
-    "If BSD constituent does not exist, create a new BSD constituent AND ground-control user with those credentials": function (browser) {
-      Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
-      Test.takeScreenshots(browser, 'call/2-call');
-
-      browser.end();
-    },
-    "If same credentials, create a new ground-control user with those credentials": function (browser) {
-      Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
-      Test.takeScreenshots(browser, 'call/2-call');
-
-      browser.end();
-    },
-    "If account credentials are correct but the password for ground-control is incorrect, update the ground-control password and log in": function (browser) {
-      Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
-      Test.takeScreenshots(browser, 'call/2-call');
-
-      browser.end();
-    },
-    "If BSD credentials are incorrect, give error message with a link to reset password via BSD": function (browser) {
-      Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
-      Test.takeScreenshots(browser, 'call/2-call');
-
-      browser.end();
-    }
+    //"If BSD constituent does not exist, create a new BSD constituent AND ground-control user with those credentials": function (browser) {
+    //  Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
+    //  Test.takeScreenshots(browser, 'call/2-call');
+    //
+    //  browser.end();
+    //},
+    //"If same credentials, create a new ground-control user with those credentials": function (browser) {
+    //  Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
+    //  Test.takeScreenshots(browser, 'call/2-call');
+    //
+    //  browser.end();
+    //},
+    //"If account credentials are correct but the password for ground-control is incorrect, update the ground-control password and log in": function (browser) {
+    //  Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
+    //  Test.takeScreenshots(browser, 'call/2-call');
+    //
+    //  browser.end();
+    //},
+    //"If BSD credentials are incorrect, give error message with a link to reset password via BSD": function (browser) {
+    //  Test.autoLogin(browser, 'call', Test.config.adminLogin, true, "call/1-login");
+    //  Test.takeScreenshots(browser, 'call/2-call');
+    //
+    //  browser.end();
+    //}
   }
 });
 
