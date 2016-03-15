8bbed71732fac2e439afec3bd9adabf339af9ef8
Jacob LeGrone <jacob@jacoblegrone.com>
2016-03-15 15:37:29 -0700

    Allow all

--- src/backend/server.js
+++ src/backend/server.js
@@ -300,9 +300,9 @@ function startApp() {
         return res.redirect(['https://', req.get('Host'), req.url].join(''))
       }
 
-      res.header("Access-Control-Allow-Origin", "http://www.bernie2016events.org")
-      res.header("Access-Control-Allow-Headers", "Content-Type")
-      res.header('Access-Control-Allow-Methods', 'POST')
+      res.header("Access-Control-Allow-Origin", "*")
+      res.header("Access-Control-Allow-Headers", "*")
+      res.header('Access-Control-Allow-Methods', '*')
       next()
     })
   }
