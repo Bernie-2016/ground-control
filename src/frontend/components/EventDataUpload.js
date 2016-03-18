a401c0896b95479d265213913b416bf3a8a5d2a9
Jacob LeGrone <jacob@jacoblegrone.com>
2016-03-17 22:09:24 -0700

    Add dropzone

--- src/frontend/components/EventDataUpload.js
+++ src/frontend/components/EventDataUpload.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Relay from 'react-relay'
 import {BernieColors, BernieText} from './styles/bernie-css'
-import {Paper, Styles} from 'material-ui'
+import {Paper, Styles, FlatButton} from 'material-ui'
 import GCForm from './forms/GCForm'
 import Form from 'react-formal'
 import EventPreview from './EventPreview'
+import Dropzone from 'react-dropzone'
 import yup from 'yup'
 import moment from 'moment'
 import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider'
@@ -33,12 +34,100 @@ class EventDataUpload extends React.Component {
 
     pageContainer: {
       margin: '1rem'
+    },
+
+    dropzoneStyle: {
+      font: BernieText.default,
+      fontSize: '1.5em',
+      color: BernieColors.green,
+      margin: '0 auto',
+      width: '100%',
+      minHeight: 300,
+      borderWidth: 2,
+      borderColor: BernieColors.green,
+      borderStyle: 'dashed',
+      borderRadius: 5,
+      position: 'relative',
+      textAlign: 'center',
+      overflow: 'scroll'
+    },
+
+    dropzoneActiveStyle: {
+      borderStyle: 'solid',
+      backgroundColor: '#eee'
+    },
+
+    dropzoneRejectStyle: {
+      borderStyle: 'solid',
+      backgroundColor: '#ffdddd'
+    },
+
+    fileStatus: {
+      ...BernieText.secondaryTitle,
+      fontSize: '0.75em',
+      textAlign: 'left',
+      paddingLeft: 20
     }
   }
 
   formSchema = yup.object({
     notes: yup.string().required(),
   })
+
+  onDrop = (files) => {
+    files.forEach((file) => {
+      // Upload files
+    })
+  }
+
+  renderFileProgress() {
+    let count = 0
+    let renderErrors = (fileObj) => {
+      if (fileObj.errors.length === 0)
+        return <div></div>
+      return (
+        <div style={{
+          ...BernieText.default,
+          borderTop: '1px solid ' + BernieColors.red,
+          fontSize: '0.5em',
+          paddingLeft: 10,
+          width: 470,
+        }}>
+          {fileObj.errors.map((error) => {
+            return (
+              <div>
+                {`${error.email}, Zip: ${error.zip}, Phone: ${error.phone}`}
+                <br />
+                <span style={{color: BernieColors.red}}>{error.error}</span>
+                <hr />
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
+    let fileNodes = Object.keys(this.state.filesProcessed).map((fileName) => {
+      count = count + 1
+      let fileObj = this.state.filesProcessed[fileName]
+      let color = (fileObj.errors.length === 0 ? (fileObj.totalRows === fileObj.processedRows ? BernieColors.green : BernieColors.gray) : BernieColors.red)
+
+      return (
+        <div style={{
+          ...this.styles.fileStatus,
+          color: color,
+          backgroundColor: count % 2 ? BernieColors.lightGray : BernieColors.white
+        }}>
+          {fileName}: {fileObj.processedRows}/{fileObj.totalRows}
+          {renderErrors(fileObj)}
+        </div>
+      )
+    })
+    return (
+      <div>
+        {fileNodes}
+      </div>
+    )
+  }
 
   render() {
     const event = this.props.event
@@ -64,7 +153,7 @@ class EventDataUpload extends React.Component {
           </div>
 
           <div style={this.styles.formContainer}>
-            <Paper style={{padding: 15}}>
+            <div style={{padding: 15}}>
               <GCForm
                 schema={this.formSchema}
                 defaultValue={{
@@ -82,14 +171,39 @@ class EventDataUpload extends React.Component {
                   name='notes'
                   multiLine={true}
                   fullWidth={true}
-                  rows={5}
                   label="Notes"
                 />
+
+                <Dropzone
+                  ref="dropzone"
+                  onDrop={this.onDrop}
+                  style={this.styles.dropzoneStyle}
+                  activeStyle={this.styles.dropzoneActiveStyle}
+                  rejectStyle={this.styles.dropzoneRejectStyle}
+                  disableClick={true}
+                >
+                  <div style={{
+                    paddingTop: 20,
+                    paddingBottom: 20,
+                    borderBottom: '1px dashed ' + BernieColors.green
+                  }}>
+                    <div style={{...BernieText.title, fontSize: '1.2em', color: BernieColors.green}}>
+                      Drop your Event Files here!
+                    </div>
+                    <div style={{...BernieText.default, fontSize: '0.65em'}}>
+                      <FlatButton
+                        label="Select Files to Upload"
+                        onTouchTap={() => this.refs.dropzone.open()}
+                      />
+                    </div>
+                  </div>
+                </Dropzone>
+
                 <br />
                 <br />
                 <Form.Button type='submit' label='Submit' fullWidth={true} />
               </GCForm>
-            </Paper>
+            </div>
           </div>
 
         </div>
