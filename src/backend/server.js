7b3521a4965c93538ed069d1c6643cf11a9355c3
Jacob LeGrone <jacob@jacoblegrone.com>
2016-03-24 22:19:33 -0700

    Fix typo

--- src/backend/server.js
+++ src/backend/server.js
@@ -339,7 +339,7 @@ function startApp() {
     }
   }))
 
-  app.use('/slack/callforbernie/', proxy('join-bernie-call-team.herokuapp.com/', {
+  app.use('/slack/callforbernie/', proxy('join-bernie-call-team.herokuapp.com', {
     forwardPath: function(req, res) {
       return url.parse(req.url).path
     }
