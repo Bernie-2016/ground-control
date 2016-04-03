7761d685a35afe42b1b6fd1a8c9a0cea500f343d
bahoo <jon.c.culver@gmail.com>
2016-04-03 11:24:54 -0400

    Re-enable ballot access types for NJ.

--- src/frontend/public/admin/events/static/event-types.js
+++ src/frontend/public/admin/events/static/event-types.js
@@ -53,16 +53,16 @@ var eventTypes = [
 			description: '<a href="http://bernie.to/distance-faq">Click here to view Carpool FAQs</a><p>Join me and other Bernie supporters for a road trip!</p><p>I’ll be leaving on [WRITE YOUR DEPARTURE DATE/TIME HERE] and returning on [WRITE YOUR RETURN DATE/TIME HERE].</p><p>We’re going to hit the road for Bernie to the help out in the crucial final days before the election. Bernie staff on the ground will train us and plug us into the campaign so that we can be as effective as possible.</p><p>Victory will require all of us pitching in, so sign up and let’s go on a Bernie Journey!</p>'
 		}
 	},
-	// {
-	// 	id: 'ballot-access',
-	// 	name: 'Gather Ballot Access Signatures',
-	// 	defaultValues: {
-	// 		name: 'Bernie Ballot Blast - PA Support Bernie and his Delegates and Collect Petition Signatures',
-	// 		description: 'We only have 3 weeks to get all the needed signatures to get Bernie on the Ballot and get his Delegates nominated for the Democratic Convention.  Sign up for your local events scheduled between January 26th and February 10th.  We can\'t do this without you and you can make the difference in Pennsylvania, one of the most important swing states.  Come join the movement!',
-	// 	},
-	// 	adminOnly: false,
-	// 	disabled: ['use_shifts']
-	// },
+	{
+		id: 'ballot-access',
+		name: 'Gather Ballot Access Signatures',
+		defaultValues: {
+			name: 'Bernie Ballot Blast - PA Support Bernie and his Delegates and Collect Petition Signatures',
+			description: 'We only have 3 weeks to get all the needed signatures to get Bernie on the Ballot and get his Delegates nominated for the Democratic Convention.  Sign up for your local events scheduled between January 26th and February 10th.  We can\'t do this without you and you can make the difference in Pennsylvania, one of the most important swing states.  Come join the movement!',
+		},
+		adminOnly: false,
+		disabled: ['use_shifts']
+	},
 	{
 		id: 'phonebank',
 		name: 'Phonebank',
