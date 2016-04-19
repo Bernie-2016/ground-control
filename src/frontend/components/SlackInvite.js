069d49201e95b94d2aebd82e53aea933196fbaf3
Jacob LeGrone <jacob@jacoblegrone.com>
2016-04-19 16:18:44 -0400

    Refactor invite form (closes #686 and closes #687)

--- src/frontend/components/SlackInvite.js
+++ src/frontend/components/SlackInvite.js
@@ -1,68 +1,68 @@
 import React from 'react'
-import {Paper, TextField} from 'material-ui'
-import {BernieText, BernieColors} from './styles/bernie-css';
+import {Styles, RaisedButton, FlatButton, Paper, TextField} from 'material-ui'
+import {BernieText, BernieColors} from './styles/bernie-css'
+import {BernieTheme} from './styles/bernie-theme'
 import GCForm from './forms/GCForm'
 import Form from 'react-formal'
 import yup from 'yup'
 import superagent from 'superagent'
-import {Styles} from 'material-ui'
 import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider'
-import {BernieTheme} from './styles/bernie-theme'
 import {slacks} from './data/slacks'
-
-console.log(slacks)
 
 export default class SlackInvite extends React.Component {
   state = {
     formState: 'signup',
     errorMessage: null,
+    email: null
+  }
+
+  slackTeam = {
+    ...slacks[this.props.params.team],
+    id: this.props.params.team
+  }
+
+  formSchema = yup.object({
+    email: yup.string().email().required(),
+  })
+
+  showForm = () => {
+    this.setState({formState: 'signup'})
   }
 
   clearError() {
     this.setState({ errorMessage: null })
   }
 
-  redirectToNext() {
-    this.props.history.push('/call')
+  setError(errorMessage) {
+    this.setState({errorMessage})
   }
 
-  formStates = {
-    signup: {
-      formTitle: 'Enter your email address:',
-      formSchema: yup.object({
-        email: yup.string().email().required(),
-      }),
-      formElement: (
-        <div>
-          <Form.Field
-            name='email'
-            label='E-mail Address'
-            hintText='Email'
-            type='email'
-            floatingLabelText={false}
-            fullWidth={true}
-          />
-          <br />
-        </div>
-      ),
-      onSubmit: (formState) => {
-        this.clearError()
-        superagent
-          .post('/slack-invites')
-          .send({
-              email: formState.email,
-              slackTeam: this.props.routeParams.team
-            })
-          .end((err, res) => {
-              console.log('woo')
-              if (!err) {
-                this.redirectToNext()
-              } else {
-                this.setState({errorMessage: 'Whoops! An error occurred. Please try again!'})
-              }
-            })
-      }
-    }
+  onSubmit = (formState) => {
+    this.clearError()
+    const email = formState.email
+    superagent
+      .post('/slack-invites')
+      .send({email, slackTeam: this.slackTeam.id})
+      .end((err, res) => {
+        const response = JSON.parse(res.text)
+        if (response.ok) {
+          this.setState({formState: 'submitted', email})
+        }
+        else {
+          const error = response.error || err
+          switch (error) {
+            case 'already_invited':
+              this.setError(`${email} has already received an invitation!`)
+              break
+            case 'already_in_team':
+              this.setError(`${email} is already in ${this.slackTeam.title}`)
+              break
+            default:
+              console.error(error)
+              this.setError('Whoops! An error occurred. Please try again later, or email help@berniesanders.com.')
+          }
+        }
+      })
   }
 
   styles = {
@@ -77,71 +77,67 @@ export default class SlackInvite extends React.Component {
     container: {
       padding: '40px',
       textAlign: 'center'
-    },
-    errorMessage: {
-      ...BernieText.default,
-      color: BernieColors.lightRed,
-      fontSize: '0.8em',
-      marginTop: 15,
-      textAlign: 'center'
     }
   }
 
   renderForm() {
-    let state = this.formStates[this.state.formState];
-    let formElement = state.formElement;
-    let formTitle = state.formTitle;
-    let formSchema = state.formSchema;
-    let submitHandler = state.onSubmit;
-    let errorElement = <div></div>;
-
-    if (this.state.errorMessage) {
-      errorElement = <div style={this.styles.errorMessage}>{this.state.errorMessage}</div>
-    }
-
-    return (
-      <Paper style={this.styles.signupForm}>
-        <div style={
-          {
-            ...BernieText.title,
-            color: BernieColors.white,
-            fontSize: '1.5em'
-          }}>
+    if (this.state.formState === 'signup')
+      return (
+        <Paper style={this.styles.signupForm}>
           <GCForm
-            schema={formSchema}
-            onSubmit={(formData) => {
-              submitHandler(formData)
-            }}
+            schema={this.formSchema}
+            onSubmit={this.onSubmit}
           >
-            {formTitle}
-            {errorElement}
+            <h1 style={{
+                ...BernieText.title,
+                color: BernieColors.white,
+                fontSize: '1.5em'
+            }}>
+              Enter your email address:
+            </h1>
             <Paper zDepth={0} style={{
               padding: 15,
               marginTop: 15,
               marginBottom: 15
             }}>
-              {formElement}
+              <Form.Field
+                name='email'
+                label='E-mail Address'
+                hintText='Email'
+                type='email'
+                floatingLabelText={false}
+                fullWidth={true}
+                errorText={this.state.errorMessage}
+                style={{textAlign: 'left'}}
+              />
             </Paper>
               <Form.Button
                 type='submit'
-                label='Submit'
+                label='Send Invite'
                 fullWidth={true}
               />
-              <div style={{
-                ...BernieText.default,
-                fontSize: '0.7em',
-                color: BernieColors.white,
-                marginTop: 10
-              }}>
-              </div>
           </GCForm>
+        </Paper>
+      )
+    else
+      return (
+        <div style={BernieText.default}>
+          Success! {this.state.email} has been invited to join {this.slackTeam.title}.
+          <br /><br />
+          <RaisedButton
+            label={`Go to ${this.slackTeam.title}`}
+            secondary={true}
+            linkButton={true}
+            href={`https://${this.slackTeam.id}.slack.com`}
+          />
+          <br /><br />
+          <FlatButton label="Send Another Invite" primary={true} onClick={this.showForm} />
         </div>
-      </Paper>
-    )
+      )
   }
 
   render() {
-    const team = slacks[this.props.params.team]
+    
     return (
       <MuiThemeProvider muiTheme={Styles.getMuiTheme(BernieTheme)}>
         <div style={this.styles.container} >
@@ -155,13 +151,13 @@ export default class SlackInvite extends React.Component {
             ...BernieText.title,
             marginBottom: 0
           }}>
-            Join "{team.title}"
+            Join "{this.slackTeam.title}"
           </h1>
           <p style={{
             ...BernieText.default,
             marginBottom: '1em'
           }}>
-            {team.description}
+            {this.slackTeam.description}
           </p>
           <div styles={this.styles.formContainer}>
             {this.renderForm()}
