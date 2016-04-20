550c21cb3169076fb4265f62f32f4494b626aed9
Jacob LeGrone <jacob@jacoblegrone.com>
2016-04-20 04:11:37 -0400

    Don't show unsupported slacks for now

--- src/frontend/components/data/slacks.js
+++ src/frontend/components/data/slacks.js
@@ -1,22 +1,22 @@
 export const slacks = {
-    afam4bernie: {
-        title: 'African Americans For Bernie',
-        description: 'A community of African American activists and allies'
-    },
+    // afam4bernie: {
+    //     title: 'African Americans For Bernie',
+    //     description: 'A community of African American activists and allies'
+    // },
     bernie2016states: {
         title: 'Bernie 2016 States',
         description: 'Interface with volunteers and staff in each state'
     },
     berniebuilders: {
         title: 'Bernie Builders',
-        description: 'General volunteer chatter'
+        description: 'General volunteer discussion'
     },
-    callforbernie: {
-        title: 'Call For Bernie',
-        description: 'Phonebanking assistance and chatter'
-    },
-    codersforsanders: {
-        title: 'Coders for Sanders',
-        description: 'Volunteer development projects discussion'
-    }
+    // callforbernie: {
+    //     title: 'Call For Bernie',
+    //     description: 'Phonebanking assistance and chatter'
+    // },
+    // codersforsanders: {
+    //     title: 'Coders for Sanders',
+    //     description: 'Volunteer development projects discussion'
+    // }
 }
