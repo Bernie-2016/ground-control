0b3299813016a5fcecc9cc80fe3160acf7b57829
Jacob LeGrone <git@jacoblegrone.com>
2016-07-08 15:32:35 -0400

    Remove berniebuilders, callforbernie, and bernie2016states

--- src/frontend/components/data/slacks.js
+++ src/frontend/components/data/slacks.js
@@ -3,21 +3,21 @@ export const slacks = {
     //     title: 'African Americans For Bernie',
     //     description: 'A community of African American activists and allies'
     // },
-    bernie2016states: {
-        title: 'Bernie 2016 States',
-        description: 'Interface with volunteers and staff in each state',
-        showInIndex: true
-    },
-    berniebuilders: {
-        title: 'Bernie Builders',
-        description: 'General volunteer discussion',
-        showInIndex: true
-    },
-    callforbernie: {
-        title: 'Call Team Slack',
-        description: 'Get phonebanking assistance and talk to other callers for Bernie',
-        showInIndex: true
-    },
+    // bernie2016states: {
+    //     title: 'Bernie 2016 States',
+    //     description: 'Interface with volunteers and staff in each state',
+    //     showInIndex: true
+    // },
+    // berniebuilders: {
+    //     title: 'Bernie Builders',
+    //     description: 'General volunteer discussion',
+    //     showInIndex: true
+    // },
+    // callforbernie: {
+    //     title: 'Call Team Slack',
+    //     description: 'Get phonebanking assistance and talk to other callers for Bernie',
+    //     showInIndex: true
+    // },
     codersforsanders: {
         title: 'Coders for Sanders',
         description: 'Volunteer development projects discussion',
